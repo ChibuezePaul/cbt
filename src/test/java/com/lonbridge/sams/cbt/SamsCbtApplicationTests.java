6a3d463699c04166c8d8264dbd94cee0ff78680b
Yemi Dalley <yemi.dalley@longbridgetech.com>
2020-04-29 13:30:50 +0100

    Initial commit

---
+++ src/test/java/com/lonbridge/sams/cbt/SamsCbtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lonbridge.sams.cbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamsCbtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
