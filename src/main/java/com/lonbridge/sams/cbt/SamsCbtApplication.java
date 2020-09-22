a62d3cec75d8eb8efcf5a82a8e1dc0cb8eaac591
Chibueze Paul <chibueze.paul@longbridgetech.com>
2020-09-22 22:15:31 +0100

    deployable war configurations

--- src/main/java/com/lonbridge/sams/cbt/SamsCbtApplication.java
+++ src/main/java/com/lonbridge/sams/cbt/SamsCbtApplication.java
@@ -2,14 +2,20 @@ package com.lonbridge.sams.cbt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"com.lonbridge.sams"})
-public class SamsCbtApplication {
+public class SamsCbtApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SamsCbtApplication.class, args);
     }
-
+    
+    @Override
+    protected SpringApplicationBuilder configure ( SpringApplicationBuilder builder ) {
+        return builder.sources ( SamsCbtApplication.class );
+    }
 }
